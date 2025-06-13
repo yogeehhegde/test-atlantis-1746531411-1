c57b8222795e57e023797a8b1fa77f1b4e699bed
Yogeesh Hegde <yhegde@twilio.com>
2025-06-13 18:24:40 +0530

    Add s3 resource:

---
+++ teams/plateng-iac/597915711542/us-east-1/s3_plateng-iac-s3-bucket.tf
@@ -0,0 +1,24 @@
+# module "plateng-iac-s3-bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+
+#   bucket        = "plateng-iac-s3-bucket"
+#   acl           = "private"
+#   force_destroy = true
+#   versioning = {
+#     enabled = true
+#   }
+# }
+
+
+resource "aws_s3_bucket" "plateng-iac-s3-bucket" {
+  bucket = "plateng-iac-s3-bucket"
+
+  tags = {
+    Name        = "plateng-iac-s3-bucket"
+  }
+}
+
+output "plateng-iac-s3-bucket_bucket_arn" {
+  value = aws_s3_bucket.plateng-iac-s3-bucket.arn
+  description = "The ARN of the S3 bucket plateng-iac-s3-bucket"
+}
