7703ad814cead7c21eb49edf5185b7e1d40a40e9
Yogeesh Hegde <yhegde@twilio.com>
2025-06-14 15:28:39 +0530

    Add s3 resource:

---
+++ teams/plateng-iac/597915711542/us-east-1/s3_plateng-iac-s3-buckettest-10.tf
@@ -0,0 +1,24 @@
+# module "plateng-iac-s3-buckettest-10" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+
+#   bucket        = "plateng-iac-s3-buckettest-10"
+#   acl           = "private"
+#   force_destroy = true
+#   versioning = {
+#     enabled = true
+#   }
+# }
+
+
+resource "aws_s3_bucket" "plateng-iac-s3-buckettest-10" {
+  bucket = "plateng-iac-s3-buckettest-10"
+
+  tags = {
+    Name        = "plateng-iac-s3-buckettest-10"
+  }
+}
+
+output "plateng-iac-s3-buckettest-10_bucket_arn" {
+  value = aws_s3_bucket.plateng-iac-s3-buckettest-10.arn
+  description = "The ARN of the S3 bucket plateng-iac-s3-buckettest-10"
+}
