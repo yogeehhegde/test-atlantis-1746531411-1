a13d74b57d8a257d64d45c01532db6f54b5508a9
Yogeesh Hegde <yhegde@twilio.com>
2025-06-16 14:37:46 +0530

    Add s3 resource:

---
+++ teams/plateng-iac/597915711542/us-east-1/s3_plateng-iac-s3-bucket-us-1.tf
@@ -0,0 +1,9 @@
+
+module "plateng-iac-s3-bucket-us-1" {
+  source      = "../../../../modules/s3"
+  bucket_name = "plateng-iac-s3-bucket-us-1"
+}
+
+output "plateng-iac-s3-bucket-us-1_bucket_arn" {
+  value = module.plateng-iac-s3-bucket-us-1.bucket_arn
+}
