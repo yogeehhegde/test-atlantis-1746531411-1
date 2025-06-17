70d1d2cbc5dab077bc36b63ecc6098209ff726fa
yogeehhegde <114392663+yogeehhegde@users.noreply.github.com>
2025-06-17 11:31:05 +0530

    Delete teams/plateng-iac/597915711542/us-east-2/s3_plateng-iac-s3-bucket-us-2.tf

--- teams/plateng-iac/597915711542/us-east-2/s3_plateng-iac-s3-bucket-us-2.tf
+++
@@ -1,8 +0,0 @@
-module "plateng-iac-s3-bucket-us-2" {
-  source      = "../../../../modules/s3"
-  bucket_name = "plateng-iac-s3-bucket-us-2"
-}
-
-output "plateng-iac-s3-bucket-us-2_bucket_arn" {
-  value = module.plateng-iac-s3-bucket-us-2.bucket_arn
-}
