54346e50eda519a0c3e95b3dfa217716b9c73e79
yogeehhegde <114392663+yogeehhegde@users.noreply.github.com>
2025-06-24 10:29:45 +0530

    Delete teams/plateng-iac/597915711542/us-east-1/s3_yhegde-test-bkt.tf

--- teams/plateng-iac/597915711542/us-east-1/s3_yhegde-test-bkt.tf
+++
@@ -1,8 +0,0 @@
-module "yhegde-test-bkt" {
-  source      = "../../../../modules/s3"
-  bucket_name = "yhegde-test-bkt"
-}
-
-output "yhegde-test-bkt_bucket_arn" {
-  value = module.yhegde-test-bkt.bucket_arn
-}
